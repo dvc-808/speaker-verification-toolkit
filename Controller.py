fba1e392cbe98479716b9e0d3d5e989ba0cbdc67
dvcchamhocvcl <dvcchamhoc@gmail.com>
2025-07-19 09:59:32 +0700

    simple route done

--- Controller.py
+++ Controller.py
@@ -1,11 +1,27 @@
-def enroll_controller(Inference):
+from fastapi.responses import JSONResponse
+import soundfile
+import os
+
+def preprocess_audio(file):
+    return file
+
+def check_file_format(files):
+    invalid_format_files = []
+    for i in files:
+        if(i.content_type != "audio/wav"):
+            invalid_format_files.append(i.filename)
+        
+
+def enroll_controller(model, files):
     #take request
+    
     #parse request and locate audio file
+
     #calculate embeddings
-    mean_embeds = Inference.enroll_user("audio_enroll")
+    mean_embeds = model.enroll_user("audio_enroll")
     print(mean_embeds)
-    #save new user to database
-    return {"Hello": mean_embeds}
+    #save new user to database 
+    return {"Hello": str(mean_embeds)}
 
 def verify_controller():
     return "dinhvietcuong"
--- Router.py
+++ Router.py
@@ -1,34 +1,20 @@
 from typing import Union
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
+from fastapi import FastAPI, UploadFile, File
 from Controller import *
-from os import name
 from SASVNet import *
 
-# model = object()
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-# Load the ML model
 s = SASVNet(model="MFA_Conformer")
 s = WrappedModel(s)
 Infer = Inference(s)
 Infer.loadParameters("weights/MFA_11spk_VSASV_1.model")
 model = Infer
-    # yield
-    # Clean up the ML models and release the resources
-
-# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 
-
-@app.get("/")
-async def enroll():
-    return enroll_controller(model)
+@app.post("/enroll-user")
+#the ellipsis ... means "this is required"
+async def enroll(fullname: str, age :int, files: list[UploadFile] = File(...) ):
+    return enroll_controller(model, files)
 
 @app.get("/verify")
 def verify():
     return verify_controller()
-
-if __name__ == "__main__":
-    print("hello world")
